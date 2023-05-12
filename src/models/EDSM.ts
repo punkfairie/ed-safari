535d289e4324d9ea1694fe8c39e3b721aa1082bb
punkfairie <marleyrae1997@gmail.com>
2023-05-11 22:14:44 -0700

    EDSM.request()

---
+++ src/models/EDSM.ts
@@ -0,0 +1,24 @@
+import fetch from 'electron-fetch'
+import { Log } from './Log'
+
+class EDSM {
+    constructor() {}
+
+    static async request(url: string, options: {[index: string]: string}): Promise<object|undefined> {
+        let data: object|undefined = undefined
+
+        try {
+            const response = await fetch(url + '?' + new URLSearchParams(options))
+
+            if (!response.ok) {
+                throw new Error(`Network error - ${response}`)
+            }
+
+            data = await response.json()
+        } catch (err) {
+            Log.write(`ERROR - EDSM.request(): ${err}`)
+        }
+
+        return data
+    }
+}
