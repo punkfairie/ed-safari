e34000e84e818eeca5a5568adb52287d142b95ba
punkfairie <marleyrae1997@gmail.com>
2023-05-13 12:26:25 -0700

    titlebar & close btn

--- src/main.js
+++ src/main.js
@@ -14,6 +14,8 @@ const createWindow = () => {
     mainWindow = new BrowserWindow({
         width: 1000,
         height: 800,
+        backgroundColor: 'black',
+        frame: false,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
             nodeIntegration: true,
@@ -60,60 +62,60 @@ app.on('activate', () => {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
 
-const openSettings = async () => {
-    settingsWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        parent: mainWindow,
-        modal: true,
-        show: false,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-            additionalArguments: [`EDS-ENV=${app.isPackaged}`],
-        },
-    });
+// const openSettings = async () => {
+//     settingsWindow = new BrowserWindow({
+//         width: 800,
+//         height: 600,
+//         parent: mainWindow,
+//         modal: true,
+//         show: false,
+//         webPreferences: {
+//             nodeIntegration: true,
+//             contextIsolation: false,
+//             additionalArguments: [`EDS-ENV=${app.isPackaged}`],
+//         },
+//     });
 
-    if (SETTINGS_WINDOW_VITE_DEV_SERVER_URL) {
-        settingsWindow.loadURL(`${SETTINGS_WINDOW_VITE_DEV_SERVER_URL}/settings.html`);
-    } else {
-        settingsWindow.loadFile(path.join(__dirname, `../renderer/${SETTINGS_WINDOW_VITE_NAME}/settings.html`));
-    }
+//     if (SETTINGS_WINDOW_VITE_DEV_SERVER_URL) {
+//         settingsWindow.loadURL(`${SETTINGS_WINDOW_VITE_DEV_SERVER_URL}/settings.html`);
+//     } else {
+//         settingsWindow.loadFile(path.join(__dirname, `../renderer/${SETTINGS_WINDOW_VITE_NAME}/settings.html`));
+//     }
 
-    // Open the DevTools.
-    if (!app.isPackaged) {
-        settingsWindow.webContents.openDevTools();
-    }
+//     // Open the DevTools.
+//     if (!app.isPackaged) {
+//         settingsWindow.webContents.openDevTools();
+//     }
 
-    settingsWindow.show();
+//     settingsWindow.show();
 
-    // Make sure window is destroyed on close, or else it won't open again.
-    settingsWindow.on('closed', () => {
-        settingsWindow = undefined;
-    });
-}
+//     // Make sure window is destroyed on close, or else it won't open again.
+//     settingsWindow.on('closed', () => {
+//         settingsWindow = undefined;
+//     });
+// }
 
-const menuTemplate = [
-    {
-        label: 'File',
-        submenu: [
-            { label: 'Settings', click: async () => { openSettings(); } },
-            { role: 'quit' }
-        ]
-    },
-    {
-        role: 'help',
-        submenu: [
-            {
-                label: 'Github',
-                click: async () => {
-                    const { shell } = require('electron');
-                    await shell.openExternal('https://github.com/punkfairie/ed-safari');
-                }
-            }
-        ]
-    }
-]
+// const menuTemplate = [
+//     {
+//         label: 'File',
+//         submenu: [
+//             { label: 'Settings', click: async () => { openSettings(); } },
+//             { role: 'quit' }
+//         ]
+//     },
+//     {
+//         role: 'help',
+//         submenu: [
+//             {
+//                 label: 'Github',
+//                 click: async () => {
+//                     const { shell } = require('electron');
+//                     await shell.openExternal('https://github.com/punkfairie/ed-safari');
+//                 }
+//             }
+//         ]
+//     }
+// ]
 
-const menu = Menu.buildFromTemplate(menuTemplate)
-Menu.setApplicationMenu(menu)
+// const menu = Menu.buildFromTemplate(menuTemplate)
+// Menu.setApplicationMenu(menu)
