672c665aec0ce7aab6cb674c5a79216ed3b23710
punkfairie <marleyrae1997@gmail.com>
2023-05-12 21:08:33 -0700

    Pushing to Github

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 1000,
     height: 800,
+    titleBarStyle: 'hidden',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
